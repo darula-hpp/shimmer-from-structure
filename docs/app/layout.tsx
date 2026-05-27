b199802a62e857a475644aa1da37abeee285f86a
Olebogeng Mbedzi <olebogeng350@gmail.com>
2026-05-27 18:20:24 +0200

    Update the docs (#111)
    * docs fix code width on mobile
    
    * dont scan build artifacts
    
    * chore(vue): remove debug console log from measureElements
    
    * chore: add changeset for vue debug log removal
    
    * chore: bump all packages to v2.4.6
    
    * docs - update the docs

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -56,12 +56,6 @@ export const metadata: Metadata = {
     creator: '@OlebogengMbedzi',
   },
 
-  icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon-16x16.png',
-    apple: '/apple-touch-icon.png',
-  },
-
   robots: {
     index: true,
     follow: true,
